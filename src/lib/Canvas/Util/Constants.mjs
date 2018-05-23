2495e4062af2bd1f9a574494ce44f46941f66e00
kyranet <kyradiscord@gmail.com>
2018-05-23 23:20:54 +0200

    Fixed RegExp, ESLint

--- src/lib/Canvas/Util/Constants.mjs
+++ src/lib/Canvas/Util/Constants.mjs
@@ -1,3 +1,3 @@
-export const SPLIT_METHODS = /(?<=\))\s+/;
+export const SPLIT_METHODS = /(?<=\))\s*/;
 export const METHOD_PARSE = /\.(\w+)\(([^)]*)\)/;
 export const CANVAS_HEADER = /new Canvas(?:Constructor)?\((\d{2,3}), *(\d{2,3})\)\s*/;
