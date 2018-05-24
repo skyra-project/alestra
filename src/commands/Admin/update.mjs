298f4166a96199ef0fc4a2f088abeaf43b78ec6d
kyranet <kyradiscord@gmail.com>
2018-05-24 18:48:03 +0200

    Added update command

---
+++ src/commands/Admin/update.mjs
@@ -0,0 +1,19 @@
+import { Command as KlasaCommand, util } from 'klasa';
+
+export default class Command extends KlasaCommand {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['pull'],
+			description: 'Update the bot'
+			guarded: true,
+			permissionLevel: 10,
+		});
+	}
+
+	async run(message) {
+		const { stdout, stderr } = await util.exec(`git pull github.com/kyranet/Smii.git`);
+		return message.sendCode('prolog', [stdout, stderr].join('---\n'));
+	}
+
+}
