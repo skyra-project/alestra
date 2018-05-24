35828120580761ffa17736a9d22db06c614f8a02
kyranet <kyradiscord@gmail.com>
2018-05-24 10:31:55 +0200

    Fixed constants

--- src/lib/Canvas/Util/Constants.mjs
+++ src/lib/Canvas/Util/Constants.mjs
@@ -1,4 +1,4 @@
-export const SPLIT_METHODS = /(?<=\))\s*/;
-export const METHOD_PARSE = /\.(\w+)\(([^)]*)\)/;
+export const SPLIT_METHODS = /(?<=\))\s*\./;
+export const METHOD_PARSE = /\.?(\w+)\((.*)\)$/;
 export const CANVAS_HEADER = /new Canvas(?:Constructor)?\((\d{2,3}), *(\d{2,3})\)\s*/;
 export const QUOTES = /'|"|`/;
