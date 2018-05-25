e3a420d371139f32c491fe31c7b84f14997f7323
kyranet <kyradiscord@gmail.com>
2018-05-25 22:36:52 +0200

    Fixed CANVAS_HEADER

--- src/lib/Canvas/Util/Constants.mjs
+++ src/lib/Canvas/Util/Constants.mjs
@@ -1,6 +1,6 @@
 export const SPLIT_METHODS = /(?<=\))\s*\./;
 export const METHOD_PARSE = /^\.?(\w+)\((.*)\)$/;
-export const CANVAS_HEADER = /^new Canvas(?:Constructor)?\s*\((\d(?:\.\d*)?), *(\d(?:\.\d*)?)\)\s*/;
+export const CANVAS_HEADER = /^new Canvas(?:Constructor)?\s*\((\d+(?:\.\d*)?), *(\d+(?:\.\d*)?)\)\s*/;
 export const QUOTES = /'|"|`/;
 export const NUMBER = /\d/;
 export const CHAR = /[a-zA-Z_]/;
