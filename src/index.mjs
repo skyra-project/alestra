5ceac6cd4fabc0cf03b091691bad3e2a7fa04e76
bdistin <bdistin@gmail.com>
2018-05-26 09:31:10 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,14 @@
-export Smii from './lib/Smii';
+export { default as Smii } from './lib/Smii';
 
-export Init from './lib/Canvas/Data/Init';
-export Argument from './lib/Canvas/Parser/Argument';
-export ArgumentParser from './lib/Canvas/Parser/ArgumentParser';
-export Evaluator from './lib/Canvas/Parser/Evaluator';
-export Method from './lib/Canvas/Parser/Method';
-export PropertyMap from './lib/Canvas/Parser/PropertyMap';
+export { default as Init } from './lib/Canvas/Data/Init';
+export { default as Argument } from './lib/Canvas/Parser/Argument';
+export { default as ArgumentParser } from './lib/Canvas/Parser/ArgumentParser';
+export { default as Evaluator } from './lib/Canvas/Parser/Evaluator';
+export { default as Method } from './lib/Canvas/Parser/Method';
+export { default as PropertyMap } from './lib/Canvas/Parser/PropertyMap';
 
-export * as Constants from './lib/Canvas/Util/Constants';
-export * as Util from './lib/Canvas/Util/Util';
-export * as ValidateError from './lib/Canvas/Util/ValidateError';
+import * as Constants from './lib/Canvas/Util/Constants';
+import * as Util from './lib/Canvas/Util/Util';
+import * as ValidateError from './lib/Canvas/Util/ValidateError';
+
+export { Constants, Util, ValidateError };
