a7e909b7ac429efca424d03cbb5f64fb24672563
kyraNET <kyradiscord@gmail.com>
2019-08-14 07:49:35 +0200

    build: Fixed breaking changes

--- src/lib/AlestraClient.ts
+++ src/lib/AlestraClient.ts
@@ -1,5 +1,5 @@
 import { Colors, KlasaClient, KlasaClientOptions, util } from 'klasa';
-import { Node } from 'veza';
+import { Client as VezaClient } from 'veza';
 import { IPCMonitorStore } from './structures/IPCMonitorStore';
 
 const g = new Colors({ text: 'green' }).format('[IPC   ]');
@@ -9,11 +9,9 @@ const r = new Colors({ text: 'red' }).format('[IPC   ]');
 export class AlestraClient extends KlasaClient {
 
 	public ipcMonitors = new IPCMonitorStore(this);
-	public ipc = new Node('alestra-master')
-		.on('client.identify', client => { this.console.log(`${g} Client Connected: ${client.name}`); })
-		.on('client.disconnect', client => { this.console.log(`${y} Client Disconnected: ${client.name}`); })
-		.on('client.destroy', client => { this.console.log(`${y} Client Destroyed: ${client.name}`); })
-		.on('server.ready', server => { this.console.log(`${g} Client Ready: Named ${server.name}`); })
+	public ipc = new VezaClient('alestra-master')
+		.on('disconnect', client => { this.console.log(`${y} Disconnected: ${client.name}`); })
+		.on('ready', client => { this.console.log(`${g} Ready ${client.name}`); })
 		.on('error', (error, client) => { this.console.error(`${r} Error from ${client.name}`, error); })
 		.on('message', this.ipcMonitors.run.bind(this.ipcMonitors));
 
@@ -29,6 +27,10 @@ export class AlestraClient extends KlasaClient {
 }
 
 declare module 'discord.js' {
+	interface Client {
+		ipcMonitors: IPCMonitorStore;
+		ipc: VezaClient;
+	}
 	interface ClientOptions {
 		dev?: boolean;
 	}
