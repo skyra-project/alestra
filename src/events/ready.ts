2cf3411f69c453628047b0a6d455098b7b45c713
Antonio Román <kyradiscord@gmail.com>
2020-10-29 01:38:36 +0100

    chore: clean up code

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,29 +3,49 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Event, EventOptions } from '@sapphire/framework';
 import { green, magenta, magentaBright, red, yellow, yellowBright } from 'colorette';
 
-const success = green('+');
-const failed = red('-');
-const llc = DEV ? magentaBright : yellowBright;
-const blc = DEV ? magenta : yellow;
-
 @ApplyOptions<EventOptions>({ once: true })
 export class UserEvent extends Event<'ready'> {
 	public run() {
-		this.client.logger.info(`${llc(`         ¿        `)}       __      ___       _______   ________  ___________  _______        __
-${llc(`        ▓▓¿       `)}      /""\\    |"  |     /"     "| /"       )("     _   ")/"      \\      /""\\
-${llc(`       ▓▓▓▓Ç      `)}     /    \\   ||  |    (: ______)(:   \\___/  )__/  \\\\__/|:        |    /    \\
-${llc(`      ▓▓╝╙▓▓Ç     `)}    /' /\\  \\  |:  |     \\/    |   \\___  \\       \\\\_ /   |_____/   )   /' /\\  \\
-${blc(`     ▓▓╜  ╙▓▓╕    `)}   //  __'  \\  \\  |___  // ___)_   __/  \\\\      |.  |    //      /   //  __'  \\
-${blc(`    á╣╛    └▓╣╕   `)}  /   /  \\\\  \\( \\_|:  \\(:      "| /" \\   :)     \\:  |   |:  __   \\  /   /  \\\\  \\
-${blc(`   "▓╛  ${llc(`▓▓⌐`)} "▓╜   `)} (___/    \\___)\\_______)\\_______)(_______/       \\__|   |__|  \\___)(___/    \\___)
-${llc(`      ┌▓▓▓▓╕      `)} ${VERSION.padStart(80, ' ')}
-${llc(`     ╓▓▓▓▓▓▓▄     `)} [${success}] Gateway
-${llc(`    Æ▓▓▓┘└▓▓▓▓    `)} [${Reflect.get(this.client, 'websocket') ? success : failed}] Evlyn Bridge
-${llc(`   ▓▓▓▓└  '▓▓▓▓,  `)}
-${llc(` ,▓╣╣▓      ╚╣╣▓┐ `)}
-${blc(` ╙▓▓▓W,     g▓▓▓▀ `)}
-${blc(`   └▓▓▓▓¿,φ▓▓▓└   `)}
-${blc(`     ,╚▓╣╣╣▓└     `)}${DEV ? ' DEVELOPMENT MODE' : ''}
-${blc(`        ╙╜,       `)}`);
+		const success = green('+');
+		const failed = red('-');
+		const llc = DEV ? magentaBright : yellowBright;
+		const blc = DEV ? magenta : yellow;
+
+		const line01 = llc(`         ¿        `);
+		const line02 = llc(`        ▓▓¿       `);
+		const line03 = llc(`       ▓▓▓▓Ç      `);
+		const line04 = llc(`      ▓▓╝╙▓▓Ç     `);
+		const line05 = blc(`     ▓▓╜  ╙▓▓╕    `);
+		const line06 = blc(`    á╣╛    └▓╣╕   `);
+		const line07 = blc(`   "▓╛  ${llc(`▓▓⌐`)} "▓╜   `);
+		const line08 = llc(`      ┌▓▓▓▓╕      `);
+		const line09 = llc(`     ╓▓▓▓▓▓▓▄     `);
+		const line10 = llc(`    Æ▓▓▓┘└▓▓▓▓    `);
+		const line11 = llc(`   ▓▓▓▓└  '▓▓▓▓,  `);
+		const line12 = llc(` ,▓╣╣▓      ╚╣╣▓┐ `);
+		const line13 = blc(` ╙▓▓▓W,     g▓▓▓▀ `);
+		const line14 = blc(`   └▓▓▓▓¿,φ▓▓▓└   `);
+		const line15 = blc(`     ,╚▓╣╣╣▓└     `);
+		const line16 = blc(`        ╙╜,       `);
+
+		this.client.logger.info(
+			String.raw`
+${line01}       __      ___       _______   ________  ___________  _______        __
+${line02}      /""\    |"  |     /"     "| /"       )("     _   ")/"      \      /""\
+${line03}     /    \   ||  |    (: ______)(:   \___/  )__/  \\__/|:        |    /    \
+${line04}    /' /\  \  |:  |     \/    |   \___  \       \\_ /   |_____/   )   /' /\  \
+${line05}   //  __'  \  \  |___  // ___)_   __/  \\      |.  |    //      /   //  __'  \
+${line06}  /   /  \\  \( \_|:  \(:      "| /" \   :)     \:  |   |:  __   \  /   /  \\  \
+${line07} (___/    \___)\_______)\_______)(_______/       \__|   |__|  \___)(___/    \___)
+${line08} ${llc(VERSION.padStart(80, ' '))}
+${line09} [${success}] Gateway
+${line10} [${Reflect.get(this.client, 'websocket') ? success : failed}] Evlyn Bridge
+${line11}
+${line12}
+${line13}
+${line14}
+${line15}${DEV ? ' DEVELOPMENT MODE' : ''}
+${line16}`.trim()
+		);
 	}
 }
