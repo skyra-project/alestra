ca1cf8d0f64f8dc377bda5f4c69c133f5360f674
Antonio Román <kyradiscord@gmail.com>
2020-10-29 01:26:42 +0100

    feat: dual-color banner

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,31 +1,31 @@
 import { DEV, VERSION } from '@root/config';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Event, EventOptions } from '@sapphire/framework';
-import { green, magenta, red, yellow } from 'colorette';
+import { green, magenta, magentaBright, red, yellow, yellowBright } from 'colorette';
 
 const success = green('+');
 const failed = red('-');
-const logoColor = DEV ? magenta : yellow;
+const llc = DEV ? magentaBright : yellowBright;
+const blc = DEV ? magenta : yellow;
 
 @ApplyOptions<EventOptions>({ once: true })
 export class UserEvent extends Event<'ready'> {
 	public run() {
-		this.client.logger.info(`
-${logoColor(`         ¿        `)}       __      ___       _______   ________  ___________  _______        __
-${logoColor(`        ▓▓¿       `)}      /""\\    |"  |     /"     "| /"       )("     _   ")/"      \\      /""\\
-${logoColor(`       ▓▓▓▓Ç      `)}     /    \\   ||  |    (: ______)(:   \\___/  )__/  \\\\__/|:        |    /    \\
-${logoColor(`      ▓▓╝╙▓▓Ç     `)}    /' /\\  \\  |:  |     \\/    |   \\___  \\       \\\\_ /   |_____/   )   /' /\\  \\
-${logoColor(`     ▓▓╜  ╙▓▓╕    `)}   //  __'  \\  \\  |___  // ___)_   __/  \\\\      |.  |    //      /   //  __'  \\
-${logoColor(`    á╣╛    └▓╣╕   `)}  /   /  \\\\  \\( \\_|:  \\(:      "| /" \\   :)     \\:  |   |:  __   \\  /   /  \\\\  \\
-${logoColor(`   "▓╛  ▓▓⌐ "▓╜   `)} (___/    \\___)\\_______)\\_______)(_______/       \\__|   |__|  \\___)(___/    \\___)
-${logoColor(`      ┌▓▓▓▓╕      `)} ${VERSION.padStart(80, ' ')}
-${logoColor(`     ╓▓▓▓▓▓▓▄     `)} [${success}] Gateway
-${logoColor(`    Æ▓▓▓┘└▓▓▓▓    `)} [${Reflect.get(this.client, 'websocket') ? success : failed}] Evlyn Bridge
-${logoColor(`   ▓▓▓▓└  '▓▓▓▓,  `)}
-${logoColor(` ,▓╣╣▓      ╚╣╣▓┐ `)}
-${logoColor(` ╙▓▓▓W,     g▓▓▓▀ `)}
-${logoColor(`   └▓▓▓▓¿,φ▓▓▓└   `)}
-${logoColor(`     ,╚▓╣╣╣▓└     `)}${DEV ? ' DEVELOPMENT MODE' : ''}
-${logoColor(`        ╙╜,       `)}`);
+		this.client.logger.info(`${llc(`         ¿        `)}       __      ___       _______   ________  ___________  _______        __
+${llc(`        ▓▓¿       `)}      /""\\    |"  |     /"     "| /"       )("     _   ")/"      \\      /""\\
+${llc(`       ▓▓▓▓Ç      `)}     /    \\   ||  |    (: ______)(:   \\___/  )__/  \\\\__/|:        |    /    \\
+${llc(`      ▓▓╝╙▓▓Ç     `)}    /' /\\  \\  |:  |     \\/    |   \\___  \\       \\\\_ /   |_____/   )   /' /\\  \\
+${blc(`     ▓▓╜  ╙▓▓╕    `)}   //  __'  \\  \\  |___  // ___)_   __/  \\\\      |.  |    //      /   //  __'  \\
+${blc(`    á╣╛    └▓╣╕   `)}  /   /  \\\\  \\( \\_|:  \\(:      "| /" \\   :)     \\:  |   |:  __   \\  /   /  \\\\  \\
+${blc(`   "▓╛  ${llc(`▓▓⌐`)} "▓╜   `)} (___/    \\___)\\_______)\\_______)(_______/       \\__|   |__|  \\___)(___/    \\___)
+${llc(`      ┌▓▓▓▓╕      `)} ${VERSION.padStart(80, ' ')}
+${llc(`     ╓▓▓▓▓▓▓▄     `)} [${success}] Gateway
+${llc(`    Æ▓▓▓┘└▓▓▓▓    `)} [${Reflect.get(this.client, 'websocket') ? success : failed}] Evlyn Bridge
+${llc(`   ▓▓▓▓└  '▓▓▓▓,  `)}
+${llc(` ,▓╣╣▓      ╚╣╣▓┐ `)}
+${blc(` ╙▓▓▓W,     g▓▓▓▀ `)}
+${blc(`   └▓▓▓▓¿,φ▓▓▓└   `)}
+${blc(`     ,╚▓╣╣╣▓└     `)}${DEV ? ' DEVELOPMENT MODE' : ''}
+${blc(`        ╙╜,       `)}`);
 	}
 }
